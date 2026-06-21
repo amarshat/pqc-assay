b525b3ca691d7f083ff1bebafe64295e7ed1ff1a
Amar Akshat <amar.akshat@gmail.com>
2026-06-21 23:33:13 +0100

    proof/tier2: induction-step helper lemmas (brv8_pow_add, z_closed, sum_pair_split)
    brv8_pow_add: brv8(2^s+a) = 2^(7-s)+brv s a*2^(8-s) (bitwise, via bit_brv/bit_add_push).
    z_closed: zt(a+2^s) = zeta^((2 brv_s a+1)*2^(7-s)) mod q (the symbolic twiddle).
    sum_pair_split: even/odd halves of a length-2A sum. All no sorry, Tier2 exit 0.

--- spec/isabelle/tier2/work/Negacyclic_Bridge.thy
+++ spec/isabelle/tier2/work/Negacyclic_Bridge.thy
@@ -94,4 +94,124 @@ lemma inv_form_8:
   "inv_form 8 g n = (\<Sum>m<256. g m * zr ^ ((2 * brv 8 n + 1) * m))"
   by (simp add: inv_form_def)
 
+subsection \<open>Helper lemmas for the induction step\<close>
+
+text \<open>Bit-reversal of a single top bit plus a low residue: \<open>brv\<^sub>8(2^s + a)\<close>.
+  The bit \<open>2^s\<close> reverses to weight \<open>2^(7-s)\<close>; the low \<open>s\<close> bits \<open>a\<close> reverse and
+  shift to the top, weight \<open>2^(8-s)\<close>. Proven bitwise via \<open>bit_brv\<close> / \<open>bit_add_push\<close>.\<close>
+lemma brv8_pow_add:
+  assumes s: "s < 8" and a: "a < 2^s"
+  shows "brv 8 (2^s + a) = 2^(7-s) + brv s a * 2^(8-s)"
+proof (rule bit_eqI)
+  fix j :: nat
+  \<comment> \<open>bit of the argument \<open>2^s + a\<close>: low bits from \<open>a\<close>, then the set bit at \<open>s\<close>.\<close>
+  have arg: "bit (2^s + a) i = (if i < s then bit a i else i = s)" for i
+  proof (cases "i < s")
+    case True
+    thus ?thesis using bit_add_push[OF a, of 1 i] by (simp add: add.commute)
+  next
+    case False
+    have bit1: "bit (1::nat) k = (k = 0)" for k by (auto simp: bit_simps)
+    have step: "bit (2^s + a) i = bit (1::nat) (i - s)"
+      using bit_add_push[OF a, of 1 i] False by (simp add: add.commute)
+    have "bit (1::nat) (i - s) = (i = s)" using bit1[of "i - s"] False by presburger
+    thus ?thesis using step False by simp
+  qed
+  \<comment> \<open>bit of the result \<open>2^(7-s) + brv s a * 2^(8-s)\<close>: the lone bit \<open>7-s\<close>, then
+     the reversed low part shifted up by \<open>8-s\<close>.\<close>
+  have res: "bit (2^(7-s) + brv s a * 2^(8-s)) j
+             = (if j < 8 - s then j = 7 - s else bit (brv s a) (j - (8 - s)))"
+  proof -
+    have r: "(2::nat)^(7-s) < 2^(8-s)" using s by simp
+    have "bit (2^(7-s) + brv s a * 2^(8-s)) j
+          = (if j < 8-s then bit ((2::nat)^(7-s)) j else bit (brv s a) (j-(8-s)))"
+      using bit_add_push[OF r, of "brv s a" j] by simp
+    moreover have "bit ((2::nat)^(7-s)) j = (j = 7 - s)"
+      by (auto simp: bit_simps)
+    ultimately show ?thesis by simp
+  qed
+  show "bit (brv 8 (2^s + a)) j = bit (2^(7-s) + brv s a * 2^(8-s)) j"
+  proof (cases "j < 8")
+    case True
+    have "bit (brv 8 (2^s + a)) j = bit (2^s + a) (7 - j)"
+      using True by (simp add: bit_brv)
+    also have "\<dots> = (if 7 - j < s then bit a (7 - j) else 7 - j = s)" by (subst arg) simp
+    finally have L: "bit (brv 8 (2^s + a)) j = (if 7-j < s then bit a (7-j) else 7-j = s)" .
+    show ?thesis
+    proof (cases "j < 8 - s")
+      case True
+      \<comment> \<open>here \<open>7-j \<ge> s\<close>, so the argument bit is the lone bit-test \<open>7-j = s\<close>,
+         i.e. \<open>j = 7-s\<close>; matches the lone result bit.\<close>
+      have "\<not> 7 - j < s" using True s by linarith
+      hence "bit (brv 8 (2^s+a)) j = (7 - j = s)" using L by simp
+      moreover have "(7 - j = s) = (j = 7 - s)" using True s \<open>j<8\<close> by linarith
+      ultimately show ?thesis using True res by simp
+    next
+      case False
+      \<comment> \<open>here \<open>7-j < s\<close>, so the argument bit is \<open>bit a (7-j)\<close>; on the result side
+         it is \<open>bit (brv s a) (j-(8-s))\<close>; relate via \<open>bit_brv\<close>.\<close>
+      have hj: "7 - j < s" using False \<open>j<8\<close> s by linarith
+      hence "bit (brv 8 (2^s+a)) j = bit a (7 - j)" using L by simp
+      moreover have "bit (brv s a) (j - (8-s)) = bit a (7 - j)"
+      proof -
+        have lt: "j - (8 - s) < s" using False \<open>j<8\<close> s by linarith
+        have "bit (brv s a) (j-(8-s)) = bit a (s - Suc (j - (8-s)))"
+          using lt by (simp add: bit_brv)
+        moreover have "s - Suc (j - (8-s)) = 7 - j" using False \<open>j<8\<close> s by linarith
+        ultimately show ?thesis by simp
+      qed
+      ultimately show ?thesis using False res by simp
+    qed
+  next
+    case False
+    \<comment> \<open>\<open>j \<ge> 8\<close>: both sides are 0 (values \<open>< 256 = 2^8\<close>).\<close>
+    have "\<not> j < 8 - s" using False by simp
+    have "bit (brv 8 (2^s+a)) j = False" using False by (simp add: bit_brv)
+    moreover have "bit (2^(7-s) + brv s a * 2^(8-s)) j = bit (brv s a) (j-(8-s))"
+      using res \<open>\<not> j < 8 - s\<close> by simp
+    moreover have "brv s a < 2^s" by (rule brv_lt)
+    hence "bit (brv s a) (j - (8-s)) = False"
+      using bit_lt[of "brv s a" s "j-(8-s)"] False s by force
+    ultimately show ?thesis by simp
+  qed
+qed
+
+text \<open>The twiddle of step \<open>s\<close> at block \<open>a\<close> in closed form: \<open>zt(a + 2^s)\<close> is the
+  root to the power \<open>(2\<cdot>brv\<^sub>s a + 1)\<cdot>2^(7-s)\<close> (mod q).\<close>
+lemma z_closed:
+  assumes s: "s < 8" and a: "a < 2^s"
+  shows "[zt (a + 2^s) = zr ^ ((2 * brv s a + 1) * 2^(7-s))] (mod qq)"
+proof -
+  have xb: "(2::nat)^s \<le> 128" using s power_increasing[of s 7 "2::nat"] by simp
+  have lt: "a + 2^s < 256" using a xb by linarith
+  have exp: "brv 8 (a + 2^s) = (2 * brv s a + 1) * 2^(7-s)"
+  proof -
+    have "brv 8 (a + 2^s) = 2^(7-s) + brv s a * 2^(8-s)"
+      using brv8_pow_add[OF s a] by (simp add: add.commute)
+    also have "(8::nat) - s = Suc (7 - s)" using s by linarith
+    hence "2^(7-s) + brv s a * 2^(8-s) = (2 * brv s a + 1) * 2^(7-s)" by simp
+    finally show ?thesis .
+  qed
+  have "zt (a + 2^s) = (zr ^ (brv 8 (a + 2^s))) mod qq" by (rule zt_pow[OF lt])
+  also have "\<dots> = (zr ^ ((2 * brv s a + 1) * 2^(7-s))) mod qq" by (simp add: exp)
+  finally show ?thesis by (simp add: cong_def)
+qed
+
+text \<open>Splitting a length-\<open>2A\<close> sum into its even- and odd-indexed halves.\<close>
+lemma sum_pair_split:
+  fixes f :: "nat \<Rightarrow> 'a::comm_monoid_add"
+  shows "(\<Sum>m<2*A. f m) = (\<Sum>m<A. f (2*m)) + (\<Sum>m<A. f (2*m+1))"
+proof (induct A)
+  case 0 show ?case by simp
+next
+  case (Suc A)
+  have "(\<Sum>m<2*Suc A. f m) = (\<Sum>m<2*A. f m) + f (2*A) + f (2*A+1)"
+    by (simp add: mult_Suc_right)
+  also have "\<dots> = ((\<Sum>m<A. f (2*m)) + (\<Sum>m<A. f (2*m+1))) + f (2*A) + f (2*A+1)"
+    by (simp add: Suc.hyps)
+  also have "\<dots> = (\<Sum>m<Suc A. f (2*m)) + (\<Sum>m<Suc A. f (2*m+1))"
+    by (simp add: add.assoc add.left_commute)
+  finally show ?case .
+qed
+
 end
